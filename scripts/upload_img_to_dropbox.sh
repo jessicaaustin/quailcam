132cef206d9adb17de4e4d91c641bfe91d7e31ad
Jessica Austin <jessica@aus10.org>
2015-01-29 21:33:23 -0900

    upload smaller latest jpg, and start saving for timelapse

--- scripts/upload_img_to_dropbox.sh
+++ scripts/upload_img_to_dropbox.sh
@@ -5,7 +5,17 @@ echo "upload_img_to_dropbox: $(date):"
 echo "---------------------------------------------------"
 
 LOCALFILE=/home/pi/videos/latest.jpg
-REMOTEFILE=$(date +%Y)/$(date +%m)/$(date +%d)/$(date +%Y%m%d_%H%M).jpg
 
+# large image
+REMOTEFILE=$(date +%Y)/$(date +%m)/$(date +%d)/$(date +%Y%m%d_%H%M).jpg
 echo "Uploading ${LOCALFILE} to ${REMOTEFILE}"
 /home/pi/Dropbox-Uploader/dropbox_uploader.sh upload ${LOCALFILE} ${REMOTEFILE}
+
+# medium image (for website)
+annotation=$(date '+%Y-%m-%d %H:%M')
+convert ${LOCALFILE} -resize 25%  -fill '#eeeeee'  -undercolor '#00000080' -pointsize 12  -gravity SouthEast -annotate +5+5 " $annotation " /tmp/latest.jpg
+/home/pi/Dropbox-Uploader/dropbox_uploader.sh upload /tmp/latest.jpg /latest.jpg
+
+# small image (for timelapse)
+convert ${LOCALFILE} -resize 10% -fill '#000000'  -pointsize 12  -annotate +0+12 " $annotation " /home/pi/videos/dailytimelapse/$(date +%H%M).jpg
+# TODO: convert this to an animated gif throughout the day: convert -delay 20 -loop 0 *.jpg result.gif
